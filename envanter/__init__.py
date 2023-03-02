691494339ead65c627bb6f8f7971472a2b0f7b46
şuayip üzülmez <suayip.541@gmail.com>
2023-03-03 00:06:33 +0300

    Add changeset for v1.1.0

--- envanter/__init__.py
+++ envanter/__init__.py
@@ -1,22 +1,101 @@
+from __future__ import annotations
+
 import decimal
 import json
 import os
+from typing import TypeVar, List, Callable, Any, cast
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __all__ = ["env", "EnvironmentParser"]
 
-_empty = object()
-
-
-def _construct(parser):
-    def process(self, name, default=_empty):
-        return self.parse(name, default, parser)
-
-    return process
+_empty: Any = object()
+_T = TypeVar("_T")
+_str_T = str
+_str_F = cast(Callable[..., _T], str)
 
 
 class EnvironmentParser:
-    def parse(self, name, default=_empty, parser=None):  # noqa
+    """
+    Parses environment variables.
+    """
+
+    def parse(
+        self,
+        name: _str_T,
+        /,
+        default: _T = _empty,
+        parser: Callable[..., _T] = _str_F,
+    ) -> _T:
+        """
+        Allows fetching values from environment with custom parser
+        function to modify it before returning.
+
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found.
+        :param parser: A callable object that is going to parse the
+         variable.
+        :return: Parsed environment variable or the default value.
+        """
+        try:
+            value = os.environ[name]
+        except KeyError:
+            if default is _empty:
+                raise
+            return default
+        return parser(value)
+
+    def list(
+        self,
+        name: _str_T,
+        /,
+        default: List[Any] = _empty,
+        *,
+        delimiter: _str_T = ",",
+    ) -> List[Any]:
+        """
+        Derive a list from the value of an environment variable.
+
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found.
+        :param delimiter: Specify a string with which the variable will
+         be separated. By default, a comma is used, for example
+         'hello,world' would yield a list with 2 members.
+        :return: A list of strings or the default value.
+        """
+        try:
+            value = os.environ[name]
+        except KeyError:
+            if default is _empty:
+                raise
+            return default
+        return value.split(delimiter)
+
+    def choice(
+        self,
+        name: _str_T,
+        /,
+        default: _T = _empty,
+        *,
+        choices: List[_str_T],
+        parser: Callable[..., _T] = _str_F,
+    ) -> _T:
+        """
+        Get an environment variable, provided that it complies with the
+        choices in the related parameter. Otherwise, throws an exception
+        (ValueError), with available choices.
+
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found. Notice: if the variable is found but, it does not
+         match any of the choices, an exception will be raised.
+        :param choices: List of string that contain valid choices.
+        :param parser: A callable object that is going to parse the
+         variable, optional.
+        :return: The parsed value of environment variable.
+        """
+
         try:
             value = os.environ[name]
         except KeyError:
@@ -24,31 +103,134 @@ class EnvironmentParser:
                 raise
             return default
 
-        if parser is not None:
-            return parser(value)
+        if value not in choices:
+            raise ValueError(
+                "Got invalid value (%(value)s) from environment,"
+                " was expecting one of these: %(expected)s"
+                % {
+                    "value": value,
+                    "expected": tuple(choices),
+                }
+            )
+        return parser(value)
 
-        return value
+    def bool(self, name: _str_T, /, default: _T = _empty) -> bool | _T:
+        """
+        Get a boolean from environment variable. Allowed values are:
+        'true', '1', 'false' and '0'.
 
-    def list(self, name, default=_empty, *, delimiter=",") -> list:
-        return self.parse(name, default, lambda v: v.split(delimiter))
-
-    def bool(self, name, default=_empty) -> bool:  # noqa
-        value = self.parse(name, default=default)
-
-        if isinstance(value, bool):
-            return value
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found.
+        :return: A boolean or the default value.
+        """
+        try:
+            value = os.environ[name]
+        except KeyError:
+            if default is _empty:
+                raise
+            return default
 
         value = value.lower()
         truthy, falsy = ("true", "1"), ("false", "0")
         allowed = truthy + falsy
-        assert value in allowed, "Allowed values are: %s" % str(allowed)
+
+        if value not in allowed:
+            raise ValueError(
+                "Got invalid value (%(value)s) from environment,"
+                " was expecting one of these: %(expected)s"
+                % {
+                    "value": value,
+                    "expected": truthy + falsy,
+                }
+            )
         return value in truthy
 
-    str = _construct(str)
-    int = _construct(int)
-    float = _construct(float)
-    decimal = _construct(decimal.Decimal)
-    json = _construct(json.loads)
+    def str(self, name: _str_T, /, default: _T = _empty) -> _str_T | _T:
+        """
+        Get a string from environment.
+
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found.
+        :return: A string or the default value.
+        """
+        try:
+            return os.environ[name]
+        except KeyError:
+            if default is _empty:
+                raise
+            return default
+
+    def int(self, name: _str_T, /, default: _T = _empty) -> int | _T:
+        """
+        Get an integer from environment.
+
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found.
+        :return: An integer or the default value.
+        """
+        try:
+            return int(os.environ[name])
+        except KeyError:
+            if default is _empty:
+                raise
+            return default
+
+    def float(self, name: _str_T, /, default: _T = _empty) -> float | _T:
+        """
+        Get a float from environment.
+
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found.
+        :return: A float or the default value.
+        """
+
+        try:
+            return float(os.environ[name])
+        except KeyError:
+            if default is _empty:
+                raise
+            return default
+
+    def decimal(
+        self, name: _str_T, /, default: _T = _empty
+    ) -> decimal.Decimal | _T:
+        """
+        Get a decimal (decimal.Decimal) from environment.
+
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found.
+        :return: A decimal or the default value.
+        """
+
+        try:
+            return decimal.Decimal(os.environ[name])
+        except KeyError:
+            if default is _empty:
+                raise
+            return default
+
+    def json(self, name: _str_T, /, default: _T = _empty) -> Any | _T:
+        """
+        Get Python serialization of a JSON string from environment.
+
+        :param name: Name of the environment variable.
+        :param default: Optional default value in case the variable is
+         not found.
+        :return: The Python serialized version of the JSON,
+         or the default value.
+        """
+
+        try:
+            return json.loads(os.environ[name])
+        except KeyError:
+            if default is _empty:
+                raise
+            return default
 
 
 env = EnvironmentParser()
